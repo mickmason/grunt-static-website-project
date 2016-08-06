a284a910403332b9b22c4ac02dc6eb9e217e27de
Michael Mason <michaelmason@Michaels-MacBook-Pro.local>
2016-08-06 20:57:42 +0100

    Adding grunt contrib sass config!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,18 @@
 module.exports = function(grunt){
+    
+    grunt.initConfig({
+      sass: {                              // Task
+        dist: {                            // Target
+          options: {                       // Target options
+            style: 'expanded'
+          },
+          files: {                         // Dictionary of files
+            '/css/main.css': '/scss/main.scss',       // 'destination': 'source'
+            '/css/widgets.css': '/scss/widgets.scss'
+          }
+        }
+      }
+    });
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.registerTask('default', ['sass']);
 };
