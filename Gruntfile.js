34d9dba5edea2d22d30c31ea1e478a27152464c9
Michael Mason <michaelmason@Michaels-MacBook-Pro.local>
2016-08-13 23:35:08 +0100

    Setting up grunt contrib watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,31 @@
 module.exports = function(grunt){
     
     grunt.initConfig({
+      watch: {
+          scripts: {
+            files: '**/*.scss',
+            tasks: ['sass']    
+          }
+          
+      },
       sass: {                              // Task
         dist: {                            // Target
           options: {                       // Target options
             style: 'expanded'
           },
-          files: {                         // Dictionary of files
-            '/css/main.css': '/scss/main.scss',       // 'destination': 'source'
-            '/css/widgets.css': '/scss/widgets.scss'
-          }
+            
+          files: [{                         // Dictionary of files
+            expand: true,
+            cwd: 'dev/',
+            dest: 'dev/css/',
+            ext: '.css',
+            flatten: true,
+            src: ['scss/*.scss']       // 'destination': 'source'
+          }]
         }
       }
     });
+    grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['watch']);
 };
